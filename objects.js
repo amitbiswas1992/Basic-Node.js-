e73e5df37af7ec4396654b1b6c2ff7c53675b5ff
Amit K Biswas <biswas1992@hotmail.com>
2017-03-02 13:31:55 -0500

    added a object and return it

--- objects.js
+++ objects.js
@@ -1,10 +1,41 @@
 var pet = {
-	name: 'Patches',
-	type: 'Dog'
+    name: 'Patches',
+    type: 'Dog'
 };
 
-function printPet (pet) {
-	console.log('Your ' + pet.type + ' is named ' + pet.name);
+function printPet(pet) {
+    console.log('Your ' + pet.type + ' is named ' + pet.name);
 }
 
 printPet(pet);
+
+var person = {
+    name: "Jack",
+    age: 23,
+    speackname: function(aName) {
+        return "hello" + aName;
+    }
+};
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.speackname("herrey"));
+
+
+var car = {
+
+    tire: 4,
+    mirror: 3,
+    fuel: function(FUEL) {
+        var oil = true;
+        if (oil == true) {
+            console.log("Car going to move");
+
+        } else {
+            console.log("Car is stuck ");
+        }
+        return "here is the result" + FUEL;
+    }
+}
+
+console.log(car.fuel("Fuel is full "));
