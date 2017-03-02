0103f0715f0b69682f2bf03e9ac3be6f38d5528a
Amit K Biswas <biswas1992@hotmail.com>
2017-03-02 11:35:47 -0500

    worked with numbers

--- numbers.js
+++ numbers.js
@@ -4,3 +4,30 @@ var testThree = 90;
 var averageGrade = (testOne + testTwo + testThree) / 3;
 
 console.log('Your average grade is: ' + averageGrade);
+
+
+var val1 = 5.5;
+val2 = 5;
+
+var sum = val1 + val2;
+console.log("Addition:" + sum);
+
+var letsDrive = 15 / 5;
+console.log("devision:" + letsDrive);
+
+var multiply = 6 * 7;
+console.log(multiply);
+
+var subtract = 100 - 77;
+console.log(subtract);
+
+var reminder = 15 % 4;
+console.log(reminder);
+
+var val3 = 2;
+var val4 = 2;
+var increment1 = val3++;
+var increment2 = ++val4;
+
+console.log("increment1:" + increment1);
+console.log("increment2:" + increment2);
